13143c8e5958ac4521eabd1d9525088006bfad39
Emmanuel Oduro <emmarthurson@gmail.com>
2018-08-30 12:17:37 +0000

    Connection fix

--- src/Mpociot/Couchbase/CouchbaseServiceProvider.php
+++ src/Mpociot/Couchbase/CouchbaseServiceProvider.php
@@ -1,10 +1,10 @@
 <?php declare(strict_types=1);
 
-namespace Mpociot\Couchbase;
-
 use Illuminate\Support\ServiceProvider;
 use Mpociot\Couchbase\Eloquent\Model;
-
+use Mpociot\Couchbase\Connection as CouchbaseConnection;
+use Illuminate\Database\Connection as IlluminateConnection;
+use Mpociot\Couchbase\Listeners\MigrateListener;
 class CouchbaseServiceProvider extends ServiceProvider
 {
     /**
@@ -13,24 +13,27 @@ class CouchbaseServiceProvider extends ServiceProvider
     public function boot()
     {
         Model::setConnectionResolver($this->app['db']);
-
         Model::setEventDispatcher($this->app['events']);
     }
-
     /**
      * Register the service provider.
      */
     public function register()
     {
         // Add database driver.
-        $this->app->singleton('couchbase.connection', function ($app) {
-            $connectionName = config('database.connections.' . config('database.default'));
-            return new Connection($connectionName);
+        $this->app->singleton('couchbase.connection', function($app){
+            $config = config('database.connections.couchbase');
+            return new CouchbaseConnection($config);
+        });
+        IlluminateConnection::resolverFor('couchbase', function ($config) {
+            return app('couchbase.connection');
         });
         $this->app->resolving('db', function ($db) {
             $db->extend('couchbase', function ($config) {
                 return app('couchbase.connection');
             });
         });
+        //Add migrate listeners
+        $this->app['events']->listen(["\Illuminate\Console\Events\CommandStarting"], MigrateListener::class);
     }
 }
